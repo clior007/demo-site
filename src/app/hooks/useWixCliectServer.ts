ce5f0619a9d180f07bd2ef79e4065282ec829d2f
Lior Cohen <lior.cohen@ride.vision>
2024-10-06 17:54:40 +0300

    demo site - compilation fixes

--- src/app/hooks/useWixCliectServer.ts
+++ src/app/hooks/useWixCliectServer.ts
@@ -1,8 +1,8 @@
 import {createClient, OAuthStrategy} from '@wix/sdk';
 
-export useWixClient = () => {
+export const useWixClient = () => {
     const wixClient = createClient({
-        auth:  new OAuthStrategy({
+        auth:  OAuthStrategy({
             clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID
         })
     });
