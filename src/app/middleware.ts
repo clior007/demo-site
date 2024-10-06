93f89992add2b7d7fa01b1aeb592af76fedbb887
Lior Cohen <lior.cohen@ride.vision>
2024-10-06 20:29:51 +0300

    demo site - adding middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -8,9 +8,9 @@ export async function middleware(request: NextRequest) {
             auth: OAuthStrategy({clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!})
         });
         response.cookies.set(
-            'session',
-            JSON.stringify(await myWixClient.auth.generateVisitorTokens())
-        );
+              "session",
+              JSON.stringify(await myWixClient.auth.generateVisitorTokens()),
+            );
         return response;
     }
 }
