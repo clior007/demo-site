fb1fcec3e53cf4687a5bfc0934aa54cb70d8134c
Lior Cohen <lior.cohen@ride.vision>
2024-10-06 17:32:25 +0300

    demo site

---
+++ src/app/hooks/useWixCliectServer.ts
@@ -0,0 +1,11 @@
+import {createClient, OAuthStrategy} from '@wix/sdk';
+
+export useWixClient = () => {
+    const wixClient = createClient({
+        auth:  new OAuthStrategy({
+            clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID
+        })
+    });
+    return wixClient;
+}
+
